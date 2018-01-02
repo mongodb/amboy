a9efab6a880ab55a2456481029bd538c4ec0f2a2
Sam Kleinman <garen@tychoish.com>
2018-01-02 17:50:47 -0500

    MAKE-327: add support for a blocking wait of amboy queues (#18)

--- pool/local.go
+++ pool/local.go
@@ -10,6 +10,7 @@ package pool
 import (
 	"context"
 	"errors"
+	"sync"
 
 	"github.com/mongodb/amboy"
 	"github.com/mongodb/grip"
@@ -40,6 +41,7 @@ type localWorkers struct {
 	started  bool
 	queue    amboy.Queue
 	canceler context.CancelFunc
+	wg       sync.WaitGroup
 }
 
 // SetQueue allows callers to inject alternate amboy.Queue objects into
@@ -60,10 +62,11 @@ func (r *localWorkers) Started() bool {
 	return r.started
 }
 
-func startWorkerServer(ctx context.Context, q amboy.Queue) <-chan amboy.Job {
+func startWorkerServer(ctx context.Context, q amboy.Queue, wg *sync.WaitGroup) <-chan amboy.Job {
 	output := make(chan amboy.Job)
-
 	go func() {
+		wg.Add(1)
+		defer wg.Done()
 		for {
 			select {
 			case <-ctx.Done():
@@ -88,12 +91,14 @@ func startWorkerServer(ctx context.Context, q amboy.Queue) <-chan amboy.Job {
 	return output
 }
 
-func worker(ctx context.Context, jobs <-chan amboy.Job, q amboy.Queue) {
+func worker(ctx context.Context, jobs <-chan amboy.Job, q amboy.Queue, wg *sync.WaitGroup) {
 	var (
 		err error
 		job amboy.Job
 	)
 
+	wg.Add(1)
+	defer wg.Done()
 	defer func() {
 		// if we hit a panic we want to add an error to the job;
 		err = recovery.HandlePanicWithError(recover(), nil, "worker process encountered error")
@@ -103,7 +108,7 @@ func worker(ctx context.Context, jobs <-chan amboy.Job, q amboy.Queue) {
 				q.Complete(ctx, job)
 			}
 			// start a replacement worker.
-			go worker(ctx, jobs, q)
+			go worker(ctx, jobs, q, wg)
 		}
 	}()
 
@@ -135,14 +140,14 @@ func (r *localWorkers) Start(ctx context.Context) error {
 
 	workerCtx, cancel := context.WithCancel(ctx)
 	r.canceler = cancel
-	jobs := startWorkerServer(workerCtx, r.queue)
+	jobs := startWorkerServer(workerCtx, r.queue, &r.wg)
 
 	r.started = true
 	grip.Debugf("running %d workers", r.size)
 
 	for w := 1; w <= r.size; w++ {
 		go func() {
-			worker(workerCtx, jobs, r.queue)
+			worker(workerCtx, jobs, r.queue, &r.wg)
 		}()
 		grip.Debugf("started worker %d of %d waiting for jobs", w, r.size)
 	}
@@ -155,4 +160,5 @@ func (r *localWorkers) Close() {
 	if r.canceler != nil {
 		r.canceler()
 	}
+	r.wg.Wait()
 }
--- pool/rate_limiting.go
+++ pool/rate_limiting.go
@@ -14,6 +14,7 @@ import (
 	"context"
 	"errors"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/mongodb/amboy"
@@ -60,6 +61,7 @@ type simpleRateLimited struct {
 	interval time.Duration
 	queue    amboy.Queue
 	canceler context.CancelFunc
+	wg       sync.WaitGroup
 }
 
 func (p *simpleRateLimited) Started() bool { return p.canceler != nil }
@@ -73,7 +75,7 @@ func (p *simpleRateLimited) Start(ctx context.Context) error {
 
 	ctx, p.canceler = context.WithCancel(ctx)
 
-	jobs := startWorkerServer(ctx, p.queue)
+	jobs := startWorkerServer(ctx, p.queue, &p.wg)
 
 	// start some threads
 	for w := 1; w <= p.size; w++ {
@@ -88,6 +90,8 @@ func (p *simpleRateLimited) worker(ctx context.Context, jobs <-chan amboy.Job) {
 		err error
 		job amboy.Job
 	)
+	p.wg.Add(1)
+	defer p.wg.Done()
 
 	defer func() {
 		err = recovery.HandlePanicWithError(recover(), nil, "worker process encountered error")
@@ -133,4 +137,5 @@ func (p *simpleRateLimited) Close() {
 	if p.canceler != nil {
 		p.canceler()
 	}
+	p.wg.Wait()
 }
