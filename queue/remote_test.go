f27da846c78c1e7af779ef629f5eb0f333777f57
Brian Samek <brian.samek@mongodb.com>
2018-08-30 10:33:08 -0400

    moar tests

--- queue/remote_test.go
+++ queue/remote_test.go
@@ -328,7 +328,7 @@ checkResults:
 	s.Equal(created-numLocked, observed, fmt.Sprintf("%+v", s.queue.Stats()))
 }
 
-func (s RemoteUnorderedSuite) TestJobStatsIterator() {
+func (s *RemoteUnorderedSuite) TestJobStatsIterator() {
 	s.require.NoError(s.queue.SetDriver(s.driver))
 
 	names := make(map[string]struct{})
@@ -351,4 +351,16 @@ func (s RemoteUnorderedSuite) TestJobStatsIterator() {
 	}
 	s.Equal(len(names), counter)
 	s.Equal(counter, 30)
+}
+
+func (s *RemoteUnorderedSuite) TestTimeInfoPersists() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	s.require.NoError(s.queue.SetDriver(s.driver))
+	j := newMockJob()
+	s.Zero(j.TimeInfo())
+	s.NoError(s.queue.Put(j))
+	go s.queue.jobServer(ctx)
+	j2 := s.queue.Next(ctx)
+	s.NotZero(j2.TimeInfo())
 }
