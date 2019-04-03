7f73dc51e6a9647f167b2940d823e126af436e7a
julianedwards <julian.edwards@mongodb.com>
2019-04-03 16:01:11 -0400

    remove line

--- queue/driver_test.go
+++ queue/driver_test.go
@@ -199,7 +199,6 @@ func (s *DriverSuite) TestSaveAndSaveStatus() {
 
 	n, err = s.driver.Get(s.ctx, name)
 	s.Require().NoError(err)
-
 	s.Equal(name, n.ID())
 	s.Equal(status.Completed, n.Status().Completed)
 	s.Equal(status.InProgress, n.Status().InProgress)
