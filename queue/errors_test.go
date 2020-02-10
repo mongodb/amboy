075b46ab1da4c1af1db9130522793efbb543705e
tycho garen <garen@tychoish.com>
2020-02-09 20:58:48 -0500

    fix queue tests

--- queue/errors_test.go
+++ queue/errors_test.go
@@ -12,6 +12,6 @@ func TestDuplicateError(t *testing.T) {
 	assert.False(t, IsDuplicateJobError(nil))
 	assert.True(t, IsDuplicateJobError(NewDuplicatJobError("err")))
 	assert.True(t, IsDuplicateJobError(NewDuplicatJobErrorf("err")))
-	assert.True(t, IsDuplicateJobError(NewDuplicatJobErrorf("err %s", err)))
+	assert.True(t, IsDuplicateJobError(NewDuplicatJobErrorf("err %s", "err")))
 	assert.True(t, IsDuplicateJobError(MakeDuplicateJobError(errors.New("err"))))
 }
