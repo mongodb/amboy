83162656be2f71181b1977463ce28c1a27429945
tycho garen <garen@tychoish.com>
2020-05-19 13:29:07 -0400

    unwind

--- registry/raw.go
+++ registry/raw.go
@@ -4,7 +4,7 @@ import mgobson "gopkg.in/mgo.v2/bson"
 
 type rawJob []byte
 
-func (j *rawJob) MarshalJSON() ([]byte, error)  { return j, nil }
+func (j rawJob) MarshalJSON() ([]byte, error)   { return j, nil }
 func (j *rawJob) UnmarshalBSON(in []byte) error { *j = in; return nil }
 func (j rawJob) MarshalBSON() ([]byte, error)   { return j, nil }
 func (j *rawJob) UnmarshalJSON(in []byte) error { *j = in; return nil }
@@ -13,7 +13,7 @@ func (j *rawJob) GetBSON() (interface{}, error) { return *j, nil }
 
 type rawDependency []byte
 
-func (d *rawDependency) MarshalJSON() ([]byte, error)  { return d, nil }
+func (d rawDependency) MarshalJSON() ([]byte, error)   { return d, nil }
 func (d *rawDependency) UnmarshalJSON(in []byte) error { *d = in; return nil }
 func (d rawDependency) MarshalBSON() ([]byte, error)   { return d, nil }
 func (d *rawDependency) UnmarshalBSON(in []byte) error { *d = in; return nil }
