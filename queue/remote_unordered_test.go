b2a14e2c511d6c24d728fd45594098c49cf44825
Kim Tao <kim.tao@mongodb.com>
2022-04-08 14:48:25 -0400

    EVG-16648: remove duplicate key check TODO (#271)

--- queue/remote_unordered_test.go
+++ queue/remote_unordered_test.go
@@ -52,9 +52,6 @@ func TestRemoteUnorderedMongoSuite(t *testing.T) {
 	suite.Run(t, tests)
 }
 
-// TODO run these same tests with different drivers by cloning the
-// above Test function and replacing the driverConstructor function.
-
 func (s *RemoteUnorderedSuite) SetupTest() {
 	s.ctx, s.cancel = context.WithCancel(context.Background())
 	var err error
