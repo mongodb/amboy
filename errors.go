21da5a1ef68e54214da6c49d67a155d162662827
Jonathan Brill <jonathan.brill@10gen.com>
2024-05-20 14:21:54 -0400

    sufficient Cause

--- errors.go
+++ errors.go
@@ -90,7 +90,7 @@ func (w *writeErrors) Error() string {
 }
 
 // Cause returns the primary cause of the WriteError. The primary cause is defined as follows:
-//   - If a non-duplicate-job error is encountered it is returned.
+//   - If a non-duplicate-job error is encountered a plain error type is returned.
 //   - If every error is a duplicate job error and at least one of them is a duplicate scope error a duplicate scope error is returned.
 //   - If every error is a duplicate job error and none of them is a duplicate scope error a duplicate job error is returned.
 func (w *writeErrors) Cause() error {
