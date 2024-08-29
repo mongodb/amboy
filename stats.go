ed2a14d97d9dae3c26ea4a55af1ced5305e4b285
Chaya Malik <chaya.malik@10gen.com>
2024-08-29 14:01:17 -0400

    cr

--- stats.go
+++ stats.go
@@ -3,9 +3,7 @@ package amboy
 import (
 	"fmt"
 
-	"github.com/mongodb/grip/level"
 	"github.com/mongodb/grip/message"
-	"github.com/pkg/errors"
 )
 
 // QueueStats is a simple structure that the Stats() method in the
@@ -23,8 +21,6 @@ type QueueStats struct {
 	Blocked   int            `bson:"blocked" json:"blocked" yaml:"blocked"`
 	Total     int            `bson:"total" json:"total" yaml:"total"`
 	Context   message.Fields `bson:"context,omitempty" json:"context,omitempty" yaml:"context,omitempty"`
-
-	priority level.Priority
 }
 
 // String prints a long form report of the queue for human consumption.
@@ -61,20 +57,6 @@ func (s QueueStats) Loggable() bool { return s.Total > 0 }
 // Raw  is part of the grip/message.Composer interface and simply
 // returns the QueueStats object.
 func (s QueueStats) Raw() interface{} { return s }
-
-// Priority is part of the grip/message.Composer interface and returns
-// the priority of the message.
-func (s QueueStats) Priority() level.Priority { return s.priority }
-
-// SetPriority  is part of the grip/message.Composer interface and
-// allows the caller to configure the piroity of the message.
-func (s *QueueStats) SetPriority(l level.Priority) error {
-	if !l.IsValid() {
-		return errors.Errorf("%s (%d) is not a valid level", l, l)
-	}
-	s.priority = l
-	return nil
-}
 
 // Annotate is part of the grip/message.Composer interface and allows
 // the logging infrastructure to inject content and context into log
